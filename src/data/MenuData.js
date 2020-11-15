532a536aa0b7cc6f6c9b42b60a0a3be7512e25cf
ChrisHadj <60141623+ChrisHadj@users.noreply.github.com>
2020-11-15 23:37:20 +0200

    first push

---
+++ src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+    {title: "About", link: "/about"},
+    {title: "Trips", link: "/Trips"},
+    {title: "Careers", link: "/Careers"},
+    {title: "Contact", link: "/Contact"},
+]
---
+++ src/data/StatsData.js
@@ -0,0 +1,36 @@
+import React from "react"
+import {GiEarthAmerica} from 'react-icons/gi'
+import {MdAirplanemodeActive, MdTimer} from 'react-icons/md'
+import {FaMoneyCheck} from 'react-icons/fa'
+
+
+export const StatsData = [
+    {
+        icon: (<GiEarthAmerica css={`
+            color: #047bf1;
+            `} />),
+        title: "over 100 destinations",
+        desc: "travel to over greece",
+    },
+    {
+        icon: (<MdAirplanemodeActive css={`
+            color: #f3a82e;
+            `} />),
+        title: "over 100 destinations",
+        desc: "travel to over greece",
+    },
+    {
+        icon: (<MdTimer css={`
+            color: #f34f2e;
+            `} />),
+        title: "over 100 destinations",
+        desc: "travel to over greece",
+    },
+    {
+        icon: (<FaMoneyCheck css={`
+            color: #3af576;
+            `} />),
+        title: "over 100 destinations",
+        desc: "travel to over greece",
+    }
+]
